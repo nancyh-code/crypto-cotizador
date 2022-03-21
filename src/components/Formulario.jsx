acbd39e4b0b1f052c2f4efa7c0843dd5f9e96f41
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-21 16:16:07 -0300

    consultando la API CryptoCompare

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
@@ -13,6 +13,7 @@ const InputSubmit = styled.input`
   text-transform: uppercase;
   font-size: 20px;
   border-radius: 5px;
+  margin-top: 30px;
   transition: background-color 0.3s ease;
   &:hover {
     background-color: #fc9b2d;
@@ -21,12 +22,23 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
-  const [SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      console.log(resultado.Data);
+    };
+    consultarAPI();
+  }, []);
 
   return (
     <form>
       <SelectMonedas />
-      {/* <SelectCriptomonedas /> */}
+
       <InputSubmit type="submit" value="Cotizar" />
     </form>
   );
