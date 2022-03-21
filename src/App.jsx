1e61fbe3fc23211518adce0e23a43806ed4aca3b
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-21 13:38:23 -0300

    styles and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import { useState } from "react";
+import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 90px;
+  margin-bottom: 50px;
+  font-size: 34px;
+  &::after {
+    content: "";
+    width: 120px;
+    height: 6px;
+    background-color: #fca311;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Desde App</h1>
-    </div>
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagenes criptomonedas" />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+    </Contenedor>
   );
 }
 
